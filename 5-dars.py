e600bd8bfddf98163fc427381f69ff31d0ab596f
Shohruh <shohruh@gmail.com>
2024-03-01 15:19:09 +0500

    bot.py faylni qo'shdim

---
+++ 5-dars.py
@@ -0,0 +1,28 @@
+# namedtuple
+
+# from collections import namedtuple
+#
+# User = namedtuple('user', 'id name lastname age email')
+#
+# users = [
+#     (1, 'Toxir', 'Toxirov', 21, 'toxir@gmail.com'),
+#     (2, 'Sobir', 'Sobirov', 23, 'sobir@gmail.com'),
+#     (3, 'Nodir', 'Toxirov', 35, 'nodir@gmail.com'),
+# ]
+#
+# for user in users:
+#     us = User(*user)
+#     print(us.id, us.name, us.lastname, us.age, us.email)
+
+#-----------------------------------------------------------------
+
+
+# from collections import namedtuple
+#
+# car = namedtuple('user', 'nomi rangi  tezligi narxi orni boshqaruv')
+#
+# cars = ('Malibu', 'Qizil', '300 km/h', 20000, 4, 'Avtomat')
+#
+# print(*cars)
+
+
---
+++ bot.py
@@ -0,0 +1,4 @@
+from telebot import TeleBot
+
+bot = TeleBot('6944110798:AAHq86eDJbAHiVDjhs4Jab--UU5PBLXzdq0')
+
